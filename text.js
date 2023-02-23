8f8712989838c1cded6ae47ecc4d06ce5f0fff5c
Hadi14 <asusn43@yahoo.com>
2023-02-23 17:57:36 +0330

    ok

--- text.js
+++ text.js
@@ -1,19 +1,18 @@
 (() => {
     'use strict'
-    document.querySelector('.needs-validation')
-        .addEventListener('submit', event => {
-            const form = event.target;
-            const nameinp = document.getElementById('nameinp');
-            let isPasswordValid = false;
-            let en = nameinp.value.trim();
-            const ptern = /^[A-Za-z]*$/;
-            isPasswordValid = ptern.test(en);
-            if (!form.checkValidity() || !isPasswordValid) {
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
                 event.preventDefault()
                 event.stopPropagation()
-                console.log('not valid')
             }
-            // else // I would expect an else here 
+
             form.classList.add('was-validated')
-        })
+        }, false)
+    })
 })()
